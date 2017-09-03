d161f33a9600b4f8c3a1b98ad943bc7b0e3ee871
Monalisa Nayak <monalisanayak107@gmail.com>
2017-09-03 23:53:44 +0530

    Successfully loaded static resources!!

---
+++ onlineshopping/src/main/java/net/mnayak2/onlineshopping/controller/PageController.java
@@ -0,0 +1,20 @@
+package net.mnayak2.onlineshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+	
+	
+	@RequestMapping(value={"/","/show","/index"})
+	public ModelAndView index()
+	{
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("greeting","Welcome to Spring MVC");
+		return mv;
+	}
+	
+
+}
